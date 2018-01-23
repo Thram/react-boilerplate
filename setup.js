513b8839c01c0d11c448632ce46ba7a0a6f78966
Thram Cortapapel <thram@Thrams-MacBook-Pro.local>
2018-01-23 17:18:45 +1300

    feat($setup): Implement custom provider for setup

--- setup.js
+++ setup.js
@@ -3,30 +3,32 @@ const inquirer = require('inquirer');
 
 const GITHUB_URL = 'https://github.com';
 
-const repoPrompt = {
-  name: 'repository',
-  message: 'Input GitHub Repository name',
-};
-const ownerPrompt = {
-  name: 'owner',
-  message: 'Input Repository owner',
+const providerPrompt = {
+  name: 'provider',
+  message: `Input Provider Url (Default: ${GITHUB_URL}):`,
 };
 
-const setRemote = (owner, repository) =>
+const ownerPrompt = {
+  name: 'owner',
+  message: 'Input Repository owner:',
+};
+
+const repoPrompt = {
+  name: 'repository',
+  message: 'Input Repository name:',
+};
+
+const setRemote = (provider = GITHUB_URL, owner, repository) =>
   new Promise((resolve, reject) => {
-    const git = spawn('git', [
-      'remote',
-      'set-url',
-      'origin',
-      `${GITHUB_URL}/${owner}/${repository}`,
-    ]);
+    const git = spawn('git', ['remote', 'set-url', 'origin', `${provider}/${owner}/${repository}`]);
     git.on('exit', resolve);
     git.on('error', reject);
   });
 
 (async () => {
-  const { repository } = await inquirer.prompt(repoPrompt);
+  const { provider } = await inquirer.prompt(providerPrompt);
   const { owner } = await inquirer.prompt(ownerPrompt);
-  await setRemote(owner, repository);
+  const { repository } = await inquirer.prompt(repoPrompt);
+  await setRemote(provider, owner, repository);
   console.log('Setup done!');
 })();
